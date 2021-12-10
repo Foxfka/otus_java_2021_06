9a3028f3aecc92091dcdc759bc34e0297d1a17b3
Fox <k.fomenko@platformaofd.ru>
2021-12-10 17:33:13 +0300

    hw06: Реализация обработки файла, сериализация, запись в файл

--- hw08-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,34 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final String fileName;
+    private final Gson gson;
 
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
+        this.gson = new Gson();
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+
+
+        try (Writer writer = new BufferedWriter(new FileWriter(fileName))) {
+            gson.toJson(data, writer);
+        } catch(IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
